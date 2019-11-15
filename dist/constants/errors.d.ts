91c8355bd423fa57f426f0eed3b3be0d017c5eca
Roman Stepochkin <r.stepochkin@solarl.ru>
2019-11-15 11:59:25 +0300

    update version 5.0.6

--- dist/constants/errors.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @hidden
- */
-export declare enum ERRORS {
-    PARSING_PARAMETERS_ERROR = "Error parsing parameters",
-    INVALID_PARAMETERS_ERROR = "Invalid parameters",
-    UNKNOWN_FUNCTION_ERROR = "Received unknown function",
-    UNKNOWN_ERROR = "Unknown error occurred"
-}
--- dist/utils/errors/ChaincodeError.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * ChaincodeError
- */
-export declare class ChaincodeError extends Error {
-    status: number;
-    constructor(message: string, status?: number);
-}
